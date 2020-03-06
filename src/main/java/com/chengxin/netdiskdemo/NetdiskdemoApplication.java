93152a0ec88b6a6bd3a703d910214eb31e93039e
程鑫 <2548949625@qq.com>
2020-03-06 16:48:33 +0800

    init

---
+++ src/main/java/com/chengxin/netdiskdemo/NetdiskdemoApplication.java
@@ -0,0 +1,13 @@
+package com.chengxin.netdiskdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetdiskdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetdiskdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chengxin/netdiskdemo/NetdiskdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chengxin.netdiskdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetdiskdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
